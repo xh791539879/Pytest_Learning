80ad8b88b479ac2ae696b787b274d9be7e219fb5
xh791539879 <791539879@qq.com>
2023-02-27 16:28:55 +0800

    生成html和allure报告

--- run_all.py
+++ run_all.py
@@ -5,31 +5,31 @@ import pytest
 if __name__ == '__main__':  ##主函数执行测试用例，执行文件夹下的所有test_文件
 
         # 指定文件夹
-        # pytest.main(['-vs','./testcaseα'])
+        # pytest.main(['-vs','./testcaseA'])
         # 指定模块
-        # pytest.main(['-vs','./testcaseα/test_zhang.py'])
+        # pytest.main(['-vs','./testcaseA/test_zhang.py'])
 
         #指定模块中的指定函数，
-        # pytest.main(['-vs','./testcaseα/test_zhang.py::test_01_func'])
+        # pytest.main(['-vs','./testcaseA/test_zhang.py::test_01_func'])
 
         # -n ,支持多线程
-        # pytest.main(['-vs','./testcaseα','-n 3'])
+        # pytest.main(['-vs','./testcaseA','-n 3'])
 
         # --reruns=2,失败重跑
-        # pytest.main(['-vs','./testcaseα/test_zhang.py','--reruns=2'])
+        # pytest.main(['-vs','./testcaseA/test_zhang.py','--reruns=2'])
 
         #-x 失败就终止运行
-        # pytest.main(['-vs','./testcaseα/test_zhang.py','-x'])
+        # pytest.main(['-vs','./testcaseA/test_zhang.py','-x'])
 
         #--maxfail=2,有两个不通过就停止运行
-        # pytest.main(['-vs','./testcaseα/test_zhang.py','--maxfail=2'])
+        # pytest.main(['-vs','./testcaseA/test_zhang.py','--maxfail=2'])
         # -k un,只执行方法名中包含‘un’的用例
-        # pytest.main(['-vs','./testcaseα/test_zhang.py','-k un'])
+        # pytest.main(['-vs','./testcaseA/test_zhang.py','-k un'])
 
         #通过打标记和配置文件，执行特定用例
         # pytest.main(['-m user or smoke'])
-        pytest.main(['./testcaseβ/user/test_user.py'])
-        os.system('allure generate ./temp -o ./report --clean')
+        pytest.main(['testcaseB/test_lan.py'])
+        os.system('allure generate ./temp -o ./AllureReport --clean')
 
 
 
---
+++ testcaseB/test_xin.py
@@ -0,0 +1,35 @@
+"""
+@pytest.fixture()装饰器实现部分用例前后置
+
+(1)scope表示的是被@pytest.fixture标记的方法的作用域。function(默认)，class类， module模块，package/session包.
+(2)params：参数化（支持，列表[]，元组()，字典列表[{},{},{}]，字典元组({},{},{})
+(3)autouse=True：自动使用，默认False
+(4)ids：当使用params参数化时，给每一个值设置一个变量名。意义不大。
+(5)name：给表示的是被@pytest.fixture标记的方法取一个别名。当取了别名之后，那么原来的名称就用不了了
+"""
+import pytest
+
+@pytest.fixture(scope='class',autouse=True)
+def my_fixture():
+    print('\n这是前置方法，可以实现部分以及全部测试用例前置')
+    yield
+    print('\n这是后置方法，可以实现部分以及全部测试用例后置')
+
+class TestXin01:
+
+    def test_xin_01(self):
+        print('\n这是test_xin01')
+
+    def test_xin_02(self):#在此处引用
+        print('\n这是test_xin02')
+
+    def test_xin_03(self):
+        print('\n这是test_xin03')
+
+class TestXin02():
+
+    def test_xin_04(self):
+        print('\n这是test_xin04')
+
+    def test_xin_05(self):
+        print('\n这是test_xin05')
