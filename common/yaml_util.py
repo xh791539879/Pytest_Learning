b6ae9d4b8b634522ed2d997182c12a20d54b878c
xh791539879 <791539879@qq.com>
2023-03-16 17:08:40 +0800

    数据驱动

--- common/yaml_util.py
+++ common/yaml_util.py
@@ -2,13 +2,20 @@
 """
 对yaml文件的操作方法
 """
+import os
+
 import yaml
 
+#获取当前根目录路径
+def get_obj_path():
+    return os.path.dirname(__file__).split('common')[0]
+
+
 #读取
-def read_yaml(key):
-    with open('../extract.yaml',mode='r',encoding='utf-8')as f:
+def read_yaml(yamlpath):
+    with open(get_obj_path() + yamlpath,mode='r',encoding='utf-8')as f:
         value = yaml.load(stream=f, Loader=yaml.FullLoader)
-        return value[key]
+        return value
 #写入
 def write_yaml(data):
     with open('../extract.yaml',mode='a',encoding='utf-8')as f:
@@ -18,3 +25,6 @@ def write_yaml(data):
 def clean_yaml():
     with open('../extract.yaml',mode='w',encoding='utf-8')as f:
         f.truncate()
+
+if __name__ == '__main__':
+    print(read_yaml('testcaseD/get_token.yaml'))
